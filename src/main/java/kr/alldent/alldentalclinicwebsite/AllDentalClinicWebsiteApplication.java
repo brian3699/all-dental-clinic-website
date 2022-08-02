4c157c0f689327cc72d05d309151a4bd5712afec
Young Jun <yj112@duke.edu>
2022-08-02 20:01:31 +0900

    initial commit

---
+++ src/main/java/kr/alldent/alldentalclinicwebsite/AllDentalClinicWebsiteApplication.java
@@ -0,0 +1,13 @@
+package kr.alldent.alldentalclinicwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllDentalClinicWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllDentalClinicWebsiteApplication.class, args);
+	}
+
+}
