d0b16715c1ef6cf67858d3ed18eec052638ebff9
Young Jun <yj112@duke.edu>
2022-08-05 19:45:23 +0900

    Created BlogPost entity class that extends BasePostEntity and adds field title #1

--- src/main/java/kr/alldent/alldentalclinicwebsite/domain/post/BlogPost.java
+++ src/main/java/kr/alldent/alldentalclinicwebsite/domain/post/BlogPost.java
@@ -1,17 +1,15 @@
 package kr.alldent.alldentalclinicwebsite.domain.post;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.util.Date;
 
 /**
- * Review is an entity class that defines the Review table
- * It extends BasePostEntity and adds fields visitDate and title.
+ * BlogPost is an entity class that defines the BlogPost table
+ * It extends BasePostEntity and adds title as a field.
  *
  * @author Young Jun
  * @version 1.0
@@ -21,24 +19,18 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Review extends BasePostEntity{
+public class BlogPost extends BasePostEntity{
 
     @Column(length = 500, nullable = false)
     private String title;
 
-    @Column(columnDefinition = "LONG", nullable = false)
-    private String visitDate;
-
     /**
-     * updates entity's title, visitDate, and body
+     * updates entity's title and body
      * @param title the new title that will replace the old one
-     * @param visitDate the new visitDate that will replace the old one
      * @param body the new body that will replace the old one
      */
-    public void update(String title, String visitDate, String body) {
+    public void update(String title, String body) {
         this.title = title;
-        this.visitDate = visitDate;
         this.body = body;
     }
-
 }
