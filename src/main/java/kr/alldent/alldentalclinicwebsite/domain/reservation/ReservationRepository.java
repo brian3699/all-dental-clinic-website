0434d5ff7792138e3662621244a8ee9f3f8653ab
Young Jun <yj112@duke.edu>
2022-08-05 21:00:35 +0900

    Created ReservationRepository that extends JpaRepository. This repository will access Reservation database #2

---
+++ src/main/java/kr/alldent/alldentalclinicwebsite/domain/reservation/ReservationRepository.java
@@ -0,0 +1,12 @@
+package kr.alldent.alldentalclinicwebsite.domain.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Optional<Reservation>> findByReservationDateTimeIsBetween(Date start, Date end);
+}
