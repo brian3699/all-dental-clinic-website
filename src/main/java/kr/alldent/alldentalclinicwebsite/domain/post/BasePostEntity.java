35f47e8db2ebbc564e7acfa8e269fe1493e668e2
Young Jun <yj112@duke.edu>
2022-08-05 19:16:41 +0900

    Created abstract entity class BasePostEntity that defines the basic fields of a post #1

---
+++ src/main/java/kr/alldent/alldentalclinicwebsite/domain/post/BasePostEntity.java
@@ -0,0 +1,51 @@
+package kr.alldent.alldentalclinicwebsite.domain.post;
+
+import kr.alldent.alldentalclinicwebsite.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+/**
+ * Class BasePostEntity is an abstract entity class that defines the basic fields of a post.
+ *
+ * @author Young Jun
+ * @version 1.0
+ * @since 2022-07-15
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public abstract class BasePostEntity extends BaseEntity {
+
+
+    @Id // Priority key of the table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String body;
+
+    private String userFirstName;
+    private String userLastName;
+
+
+    @Builder // makes this a builder pattern class
+    public BasePostEntity(String body, String userFirstName, String userLastName){
+        this.body = body;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+
+    }
+
+    /**
+     * updates entity's body
+     * @param body
+     */
+    public void update(String body) {
+        this.body = body;
+    }
+}
+
